e4f05d905c73ee1a1e44fabdd45922fe249b4e8b
Thao Chi Vu <tv2145@nyu.edu>
2023-09-16 22:35:56 -0400

    added 6 triangles and 4 rectangles

--- sketch.js
+++ sketch.js
@@ -1,5 +1,21 @@
 function setup() {
+    createCanvas(750, 750)
+    background (227,221,195)
 }
 
 function draw() {
+    fill("black")
+    triangle(0, 0, 200, 0, 0, 375)
+    rect(0, 375, 180, 375)
+    triangle(180, 375, 375, 375, 180, 750)
+    rect(180, 200, 122, 175)
+    triangle(302, 200, 302, 375, 375, 375)
+    
+    translate(750, 750)
+    triangle(0, 0, -200, 0, 0, -375)
+    rect(0, -375, -180, -375)
+    triangle(-180, -375, -375, -375, -180, -750)
+    rect(-180, -200, -122, -175)
+    triangle(-302, -200, -302, -375, -375, -375)
+
 }
